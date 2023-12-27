d178b41c56398ea753c0793222e64e05b3332f0b
Luiz Fernando <64384656+luiiizfernando@users.noreply.github.com>
2023-12-27 11:10:55 -0300

    Código dado em aula de C#
    Segue o código apresentado em aula para explicar sobre os conceitos de Propriedades, Métodos e Construtores com C#

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.Serialization;
+using ExemploExplorando.Models;
+
+Pessoa p1 = new Pessoa(nome: "Luiz", sobrenome: "Fernando");
+
+Pessoa p2 = new Pessoa(nome: "Luis",sobrenome: "Eduardo");
+
+Pessoa p3 = new Pessoa(nome: "José", sobrenome: "Eduardo");
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.AdicionarAluno(p3);
+cursoDeIngles.ListarAlunos();
+
+
+/*
+Pessoa p1 = new Pessoa();
+
+p1.Nome = "Luiz";
+p1.Sobrenome = "Fernando";
+p1.Idade = 30;
+
+p1.Apresentar();
+*/
